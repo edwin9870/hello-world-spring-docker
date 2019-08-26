e199844c305b2afdf90b467a7095eee4ce8ec503
Edwin Ramirez Ventura <edwin9870@gmail.com>
2019-08-25 22:24:16 -0400

    init project

---
+++ src/main/java/com/esmartdev/helloworldspringdocker/HelloWorldSpringDockerApplication.java
@@ -0,0 +1,13 @@
+package com.esmartdev.helloworldspringdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esmartdev/helloworldspringdocker/HelloWorldSpringDockerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.esmartdev.helloworldspringdocker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloWorldSpringDockerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
